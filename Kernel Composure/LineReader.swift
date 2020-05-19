9f564a2b9925afc3a89873ffdc84be6c86a0becf
Tyler Sparr <tyler.sparr@gmail.com>
2020-05-19 11:34:24 -0400

    Initial build of Kernel Composure

---
+++ Kernel Composure/LineReader.swift
@@ -0,0 +1,36 @@
+// Copyright © 2017 andrewwoz
+// https://github.com/andrewwoz/LineReader
+
+import Foundation
+
+/// Read text file line by line in efficient way
+public class LineReader {
+    public let path: String
+    
+    fileprivate let file: UnsafeMutablePointer<FILE>!
+    
+    init?(path: String) {
+        self.path = path
+        file = fopen(path, "r")
+        guard file != nil else { return nil }
+    }
+    
+    public var nextLine: String? {
+        var line:UnsafeMutablePointer<CChar>? = nil
+        var linecap:Int = 0
+        defer { free(line) }
+        return getline(&line, &linecap, file) > 0 ? String(cString: line!) : nil
+    }
+    
+    deinit {
+        fclose(file)
+    }
+}
+
+extension LineReader: Sequence {
+    public func  makeIterator() -> AnyIterator<String> {
+        return AnyIterator<String> {
+            return self.nextLine
+        }
+    }
+}
